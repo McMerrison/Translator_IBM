95b16aae32e41edab06521872a118e277d54464d
Talha Ehtasham <McMerrison@users.noreply.github.com>
2021-05-13 15:51:31 -0400

    Update API_KEYS.java

--- API_KEYS.java
+++ API_KEYS.java
@@ -1,8 +1,8 @@
 public class API_KEYS {
 
-  private static final String Transcription_KEY = "enfr6McE3JeAhenaS6ckd3R39lkBkCe5iXmENE5WW1yT";
-  private static final String Language_KEY = "1ZG8Jb29z7QEuBPHfDvpJxXfA2RWeAmZRlphiTdjneGc";
-  private static final String Vocalizer_KEY = "Wuy9VXoGhSUQEsKpxJdxagngiWQZIODokE5E89ts1QdB";
+  private static final String Transcription_KEY = "insert_api_key_here";
+  private static final String Language_KEY = "insert_api_key_here";
+  private static final String Vocalizer_KEY = "insert_api_key_here";
 
   public static String getT_KEY() {
     return Transcription_KEY;
